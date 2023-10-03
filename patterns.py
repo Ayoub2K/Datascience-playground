6795e411d363397c6b6914ec9055e28002f8d1af
Ayoub A <m.ayoub@live.nl>
2023-10-03 11:04:18 +0200

    samenvoegen unemployement graph

--- patterns.py
+++ patterns.py
@@ -1,57 +1,38 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-
 
 country = "USA"
 # Create a DataFrame
 df = pd.read_excel('pwt1001.xlsx', sheet_name='Data')
 df = df[df["countrycode"] == country]
-df = df[df["year"] == country]
 
-# Create subplots
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 12))
+df_unemployment = df.dropna(subset=["emp"])
+x_unemployment = df_unemployment['year']
+y_unemployment = df_unemployment['emp']
 
-# Plot population
-df = df[df["countrycode"] == country]
-df = df.dropna(subset=["pop"])
-df = df.dropna(subset=["pl_con"])
-df = df.dropna(subset=["emp"])
+# Create a figure with a single subplot
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
 
-x = df['year']
-y = df['pop']
-fit = np.polyfit(x, y, 1)  # Fit a linear trendline (1st-degree polynomial)
-axes[0].plot(x, y, marker='o', linestyle='-')
-axes[0].plot(x, fit[0] * x + fit[1], color='red', linestyle='--', label='Trendline')
-axes[0].set_ylabel('Total Population')
-axes[0].set_title('Population')
+# Plot Population and Unemployment Rate
+df_pop = df.dropna(subset=["pop"])
+x_pop = df_pop['year']
+y_pop = df_pop['pop']
+ax1.plot(x_pop, y_pop, marker='o', linestyle='-', label='Population ')
+ax1.plot(x_unemployment, y_unemployment, marker='o', linestyle='-.', color='green', label='Unemployment Rate')
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Value in millions')
+ax1.set_title('Population and Unemployment Rate  for {}'.format(country))
+ax1.legend()
 
-# Plot Inflation Rate trendline
-x = df['year']
-y = df['pl_con']
-fit = np.polyfit(x, y, 1)  # Fit a linear trendline (1st-degree polynomial)
-axes[1].plot(x, y, marker='o', linestyle='--', color='orange')
-axes[1].plot(x, fit[0] * x + fit[1], color='red', linestyle='--', label='Trendline')
-axes[1].set_ylabel('Inflation Rate')
-axes[1].set_title('Inflation Rate Trend')
+# Plot Unemployment Rate as a percentage of Population
+df_unemployment = df.dropna(subset=["emp"])
+y_unemployment_per = df_unemployment['emp'] / df_unemployment['pop'] * 100  # Calculate unemployment rate as a percentage
+ax2.plot(x_unemployment, y_unemployment_per, marker='o', linestyle='-.', color='green')
+ax2.set_ylabel('Unemployment Rate (%)')
+ax2.set_title('Unemployment Rate (% of Population) for {}'.format(country))
 
-# Plot Unemployment Rate trendline
-x = df['year']
-y = df['emp']
-fit = np.polyfit(x, y, 1)  # Fit a linear trendline (1st-degree polynomial)
-axes[2].plot(x, y, marker='o', linestyle='-.', color='green')
-axes[2].plot(x, fit[0] * x + fit[1], color='red', linestyle='--', label='Trendline')
-axes[2].set_xlabel('Year')
-axes[2].set_ylabel('Unemployment Rate')
-axes[2].set_title('Unemployment Rate Trend')
-
-# Add legends
-axes[0].legend()
-axes[1].legend()
-axes[2].legend()
-
-# Adjust layout
+# Adjust subplot spacing
 plt.tight_layout()
 
-# Show the plot
+# Show the plots
 plt.show()
